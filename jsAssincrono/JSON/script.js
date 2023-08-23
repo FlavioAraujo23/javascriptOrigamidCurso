e0270fb18e0b37cadbba317f56533973dc872697
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-08-22 21:05:19 -0300

    add lesson API e HTTP

---
+++ jsAssincrono/JSON/script.js
@@ -0,0 +1,74 @@
+// JSON
+// JavaScript Object Notation (JSON) é um formato de organização de dados, compostos por um conjunto de chave e valor. As aspas duplas são obrigatórias, tanto na chave quanto no valor quando este for uma string.
+// o json é independente da linguagem que ira interpretar ele
+ const exJson = {
+  "id": 1,
+  "nome": "Andre",
+  "email": "andre@origamid.com"
+}
+
+// valores
+// Os valores podem ser números, strings, boolean, arrays, objetos e null.
+
+const valoresJSON = {
+  "id": 1,
+  "faculdade": true,
+  "pertences": [
+    "lapis",
+    "caneta",
+    "caderno" // IMPORTANTE, jamais colocar virgula no ultimo item tanto da array ou tanto do objeto
+  ],
+  "endereco": {
+    "cidade": "Rio de Janeiro",
+    "pais": "Brasil"
+  },
+  "casado": null
+}
+
+
+// Arrays e Objetos
+// É comum possuirmos array's com objetos em cada valor da array. Cuidado para não colocar vírgula no último item do objeto ou array.
+const Json = [ // varias array´s com o valor de cada uma sendo um objeto
+  {
+    id: 1,
+    aula: 'JavaScript',
+    tempo: '25min',
+  },
+  {
+    id: 2,
+    aula: 'HTML',
+    tempo: '15min',
+  },
+  {
+    id: 3,
+    aula: 'CSS',
+    tempo: '10min',
+  },
+];
+
+
+//JSON.PARSE() E JSON.STRINGIFY()
+// JSON.parse() irá transformar um texto JSON em um objeto JavaScript. JSON.stringify() irá transformar um objeto JavaScript em uma string no formato JSON.
+const textoJSON = '{"id": 1, "titulo": "JavaScript", "tempo": "25min"}';
+const textoOBJ = JSON.parse(textoJSON); // transformando um texto JSON em um objeto JS
+
+const enderecoOBJ = {
+  cidade: 'Rio de Janeiro',
+  rua: 'Ali Perto',
+  pais: 'Brasil',
+  numero: 50,
+};
+const enderecoJSON = JSON.stringify(enderecoOBJ); // transformando um objeto em um JSON
+
+
+// exemplo real
+// Podemos guardar por exemplo no localStorage, uma string como valor de uma propriedade. E retornar essa string como um objeto.
+const configuracoes = {
+  player: 'Google API',
+  tempo: 25.5,
+  aula: '2-1 JavaScript',
+  vitalicio: true,
+};
+
+localStorage.configuracoes = JSON.stringify(configuracoes); // passo apenas uma chave com os valores de configuração
+const pegarConfiguracoes = JSON.parse(localStorage.configuracoes); // pegando as configurações
